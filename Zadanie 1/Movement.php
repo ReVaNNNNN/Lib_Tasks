9772e972f17b9d0b757eaeaa2e54ab80397b1e34
Kamil Sobecki <revannn5480@gmail.com>
2019-10-02 08:57:40 +0200

    Update Movement.php

--- Zadanie 1/Movement.php
+++ Zadanie 1/Movement.php
@@ -3,16 +3,11 @@
 
 interface Movement
 {
-    // set start speed for car
     public function go();
 
-    // stop car
     public function stop();
 
-    // accelerate car by given speed
     public function accelerate($speed);
-
-    // slow down car by given speed
+    
     public function slowdown($speed);
-
 }
