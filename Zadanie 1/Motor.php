9efe7e9e6f9675853c39e896ec5b0d582f9f725b
Kamil Sobecki <revannn5480@gmail.com>
2019-10-02 08:57:26 +0200

    Update Motor.php

--- Zadanie 1/Motor.php
+++ Zadanie 1/Motor.php
@@ -3,18 +3,13 @@
 
 abstract class Motor
 {
-    // 2 attributes - describing motor
     protected $engine_capacity;
+    
     protected $fuel_consumption;
 
-    // setting fuel consumption for motor
     abstract protected function setFuelConsumption($fuel_consumption);
 
-    // setting engine capacity for motor
     abstract protected function setEngineCapacity($engine_capacity);
 
-    // showing engine capacity for motor
     abstract protected function getEngineCapacity();
-
-
 }
