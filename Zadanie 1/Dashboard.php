4fe1b055dae0891d781c6748688dd176bc1e90eb
Kamil Sobecki <revannn5480@gmail.com>
2019-10-02 08:56:57 +0200

    Update Dashboard.php

--- Zadanie 1/Dashboard.php
+++ Zadanie 1/Dashboard.php
@@ -3,8 +3,7 @@
 
 interface Dashboard
 {
-    // show actually speed of car
     public function showSpeed();
-    // show average fuel consumption for car
+
     public function showFuelConsumption();
 }
