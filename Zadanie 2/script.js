b76d09d65797b82aca27462884f4c30074706761
Kamil Sobecki <revannn5480@gmail.com>
2019-10-20 08:27:13 +0200

    Update script.js

--- Zadanie 2/script.js
+++ Zadanie 2/script.js
@@ -4,25 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function showName() {
-
-    // creare new XMLHttpRequest
     var xhttp = new XMLHttpRequest();
 
-    // function execute after correct server response
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-
-            // show user name
             document.getElementById("userName").innerHTML =
                 this.responseText;
         }
     };
 
-    // get user name and surname from inputs
     var name = document.getElementById("name").value;
     var surname = document.getElementById("surname").value;
 
-    // send request to the server
     xhttp.open("GET", "plik.php?name=" + name + "&surname=" + surname, true);
     xhttp.send();
 }
